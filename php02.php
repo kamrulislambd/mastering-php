2d0b9f7a8a3cee1f2d2311d655e0808c27829b32
Engr. Kamrul Islam <86919619+kamrulislambd@users.noreply.github.com>
2022-07-24 22:51:57 +0600

    leap year calculate

---
+++ php02.php
@@ -0,0 +1,8 @@
+<?php
+//is leap year?
+$year = 2012;
+if ($year % 4 == 0 && ($year % 100 != 0 || $year % 400 == 0)) {
+    echo "{$year} is a leap year";
+} else {
+    echo "{$year} is not a leap year";
+}
