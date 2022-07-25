5eba07f3082961496bf193039093a26591ef5488
Engr. Kamrul Islam <86919619+kamrulislambd@users.noreply.github.com>
2022-07-25 20:48:22 +0600

    Update php02.php
    nested condition

--- php02.php
+++ php02.php
@@ -6,3 +6,18 @@ if ($year % 4 == 0 && ($year % 100 != 0 || $year % 400 == 0)) {
 } else {
     echo "{$year} is not a leap year";
 }
+
+echo "\n";
+$condition1 = true;
+$condition2 = true;
+$condition3 = false;
+
+if ($condition1 && $condition2 && $condition3) {
+    echo "Hello world";
+} elseif ($condition1 && $condition2) {
+    echo "Condition One";
+} elseif ($condition1) {
+    echo "Condition Two";
+} else {
+    echo "Condition Three";
+}
