92a1e08aeb3eff92c982b0c300253c9368f02269
Engr. Kamrul Islam <86919619+kamrulislambd@users.noreply.github.com>
2022-07-26 20:45:48 +0600

    Update factorial.php
    using space operator

--- factorial.php
+++ factorial.php
@@ -4,3 +4,17 @@ for ($i = $n, $factorial = 1; $i > 1; $i--) {
     $factorial *= $i;
 }
 printf("Factorial of %d is %d", $n, $factorial);
+
+echo PHP_EOL;
+
+$x = 17;
+$y = 15;
+
+$result = $x <=> $y;
+if ($result == 1) {
+    echo "$x is greater than $y";
+} elseif ($result == 0) {
+    echo "$x is equal to $y";
+} elseif ($result == -1) {
+    echo "$x is less than $y";
+}
