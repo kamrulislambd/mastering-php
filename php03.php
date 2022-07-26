590a0f58a82d2b09d108a793a8c81cd08a8fdd8d
Engr. Kamrul Islam <86919619+kamrulislambd@users.noreply.github.com>
2022-07-26 19:39:08 +0600

    Update php03.php
    multiple loop

--- php03.php
+++ php03.php
@@ -4,6 +4,27 @@ for ($i = 1; $i <= 5; $i++) {
     //echo $i;
     echo PHP_EOL;
     for ($j = 0; $j < $i; $j++) {
-        echo "*";
+        echo "*\n";
     }
 }
+
+//while loop
+$i = 0;
+while ($i < 10) {
+    $i++;
+    echo $i . PHP_EOL;
+}
+
+//do while loop
+$n = 0;
+do {
+    $n++;
+    echo $n . PHP_EOL;
+} while ($n < 10);
+
+//goto
+$j = 0;
+a:
+$j++;
+echo $j . PHP_EOL;
+if ($j < 5) goto a;
