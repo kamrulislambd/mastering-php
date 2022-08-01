99fb7cf1b412097e5ed2e852193192e52e8247d4
Engr. Kamrul Islam <86919619+kamrulislambd@users.noreply.github.com>
2022-08-01 22:45:11 +0600

    factorial calculation

--- externalfunction.php
+++ externalfunction.php
@@ -1,8 +1,21 @@
 <?php
+/*
 function isEven($n)
 {
     if ($n % 2 == 0) {
         return true;
     }
     return false;
+}
+*/
+function factorial($n)
+{
+    if (gettype($n) != "integer") {
+        return "Invalid";
+    }
+    $result = 1;
+    for ($i = $n; $i > 1; $i--) {
+        $result *= $i;
+    }
+    return $result;
 }
--- function.php
+++ function.php
@@ -1,8 +1,13 @@
 <?php
 include_once "externalfunction.php";
+/*
 $x = 17;
 if (isEven($x)) {
     echo "{$x} is Even Number.";
 } else {
     echo "{$x} is Odd Number.";
 }
+*/
+
+$x = 6;
+echo "Factorial of {$x} is " . factorial($x);
